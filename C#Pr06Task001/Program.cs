acf4f8d4f834d00461cd1d7dd54d0f549477c2a2
lyudmila <kudinova@alio-soft.ru>
2022-09-03 21:45:27 +0400

    Сколько чисел больше 0 ввёл пользователь

---
+++ C#Pr06Task001/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Сколько чисел Вы хотите ввести? Укажите цифру:");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+int[] ArrayNumber(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write("Введите любое натуральное число ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int PositiveArrayQuantity(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+
+int[] array = ArrayNumber(arrayLength);
+PrintArray(array);
+Console.Write($"Количество введенных положительных чисел = {PositiveArrayQuantity(array)}");
+Console.WriteLine();
